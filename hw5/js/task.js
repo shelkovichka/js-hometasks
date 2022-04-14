2c7200da672e5c56a175340e46c67918d1d3bdeb
Василиса <violettinsel@gmail.com>
2022-04-14 09:44:29 +0300

    исправлена задача 1,  добавлена задача 2

--- hw5/js/task.js
+++ hw5/js/task.js
@@ -4,10 +4,45 @@
 
 function range(start, end, step = 1) {
 	let nums = [];
-	for (start; start <= end; start += step) nums.push(start);
-	return nums;
+	if (start > end) {
+		return ('введите меньшее значение для начала диапозона');
+	} if (step > start + end) {
+		return ('введите меньшее значение для шага');
+	} if (step < 0) {
+		return ('введите положительное значение для шага');
+	} else {
+		for (start; start <= end; start += step) nums.push(start);
+		return nums;
+	}
 }
 
-console.log(range(3, 9, 2));
-console.log(range(2, 9));
+
+// 2
+
+function checkSpam(text, ...spamWords) {
+	text = text.split(' ');
+	let i = 0; 
+
+	for (let wrd of text) {
+		for (let spm of spamWords) {
+			if (spm === wrd) i++;
+		} 
+	}
+
+	if (i > 10) {
+		return('очень сильно заспамлен');
+	} else if (i > 8) {
+		return ('сильно заспамлен');
+	} else if (i > 6) {
+		return ('средне заспамлен');
+	} else if (i >= 3) {
+		return ('слабо заспамлен');
+	} else if (i < 3) {
+		return ('очень слабо заспамлен');
+	}
+}
+
+
+
+
 
